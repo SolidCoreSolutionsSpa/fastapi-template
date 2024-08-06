b940f1f3a0be3cb0ba4685fb4f6af8bc9dea3b86
Felipe Otero Luduena <felipe.otero.luduena@gmail.com>
2024-08-05 23:08:55 -0400

    black formmat

--- app/api/services/api_clients/base_service.py
+++ app/api/services/api_clients/base_service.py
@@ -2,12 +2,13 @@ from abc import ABC, abstractmethod
 from typing import Generic, TypeVar
 
 import httpx
-
-from app.core.logger import logger 
 from pydantic import BaseModel
+
+from app.core.logger import logger
 
 RequestType = TypeVar("RequestType", bound=BaseModel)
 ResponseType = TypeVar("ResponseType", bound=BaseModel)
+
 
 class BaseService(ABC, Generic[RequestType, ResponseType]):
     """
@@ -30,39 +31,36 @@ class BaseService(ABC, Generic[RequestType, ResponseType]):
         pass
 
     async def make_request(
-        self,
-        request_data: RequestType,
-        access_token: str,
-        base_url: str
+        self, request_data: RequestType, access_token: str, base_url: str
     ) -> ResponseType | None:
         """
         Realiza una solicitud HTTP GET asincrónica con los datos de la solicitud y devuelve la respuesta parseada.
-        
+
         Args:
             request_data: Datos de la solicitud.
             access_token: Token de acceso para la autenticación.
             base_url: URL base del servicio.
-        
+
         Returns:
             Una instancia de ResponseType si la solicitud y el parseo son exitosos, None en caso contrario.
-        
+
         Raises:
             HTTPStatusError: Si ocurre un error HTTP durante la solicitud.
             Exception: Para cualquier otro tipo de error.
         """
-        endpoint = self.get_endpoint()  
-        url = f"{base_url}{endpoint}"  
-        headers = {"Authorization": f"Bearer {access_token}"}  
-        params = request_data.model_dump(by_alias=True) 
+        endpoint = self.get_endpoint()
+        url = f"{base_url}{endpoint}"
+        headers = {"Authorization": f"Bearer {access_token}"}
+        params = request_data.model_dump(by_alias=True)
         async with httpx.AsyncClient() as client:
             try:
                 response = await client.get(
                     url,
                     headers=headers,
                     params=params,
-                    timeout=15, 
+                    timeout=15,
                 )
-                response.raise_for_status() 
+                response.raise_for_status()
             except httpx.HTTPStatusError as exc:
                 logger.error(
                     f"Error response {exc.response.status_code} while making request to {url}. Response content: {exc.response.content}"
@@ -74,8 +72,10 @@ class BaseService(ABC, Generic[RequestType, ResponseType]):
                 )
                 raise exc
 
-            logger.info(f"{self.__class__.__name__} with params {params} request successful")
-            response_data = response.json() 
+            logger.info(
+                f"{self.__class__.__name__} with params {params} request successful"
+            )
+            response_data = response.json()
             try:
                 return self.parse_response(response_data)
             except Exception as exc:
--- app/core/logger.py
+++ app/core/logger.py
@@ -6,7 +6,7 @@ logger.setLevel(logging.INFO)
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
 
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 console_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
